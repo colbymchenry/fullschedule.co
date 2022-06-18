58ab80f22a86da54b69fbeb6449ecf24f77ce17f
Colby McHenry <me@colbymchenry.com>
2022-06-18 08:46:53 -0400

    More CSS edits to appointments page.

--- pages/admin/dashboard/index.js
+++ pages/admin/dashboard/index.js
@@ -1,9 +1,16 @@
-import React, {forwardRef, useEffect, useRef, useState} from 'react';
+import React, {forwardRef, useEffect, useState} from 'react';
 import {APIConnector} from "../../../components/APIConnector";
 import {AuthProvider, useAuth} from "../../../context/AuthContext";
 import styles from "./styles.module.css";
-import {Button, DatePicker, Loader, Placeholder, Tag, toaster, Whisper} from "rsuite";
-import {faChevronLeft, faChevronRight, faEllipsis, faSms} from "@fortawesome/free-solid-svg-icons";
+import {Button, DatePicker, Loader, Tag, toaster, Whisper} from "rsuite";
+import {
+    faCheckCircle,
+    faChevronLeft,
+    faChevronRight,
+    faCog,
+    faCogs,
+    faEllipsis, faExclamationTriangle
+} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {TimeHelper} from "../../../utils/TimeHelper";
 import Overlay from "rsuite/Overlay";
@@ -28,6 +35,14 @@ export default function DashboardAppointments(props) {
             });
 
             setAppointments(res.data);
+
+            if (document.getElementById("current-time-line")) {
+
+                document.getElementsByClassName("page-root")[0].scrollTo({
+                    top: document.getElementById("current-time-line").offsetTop - 200,
+                    behavior: 'smooth'
+                });
+            }
         } catch (err) {
 
         }
@@ -89,8 +104,8 @@ export default function DashboardAppointments(props) {
     const isLunchBlock = (staff_id, timeSlot) => {
         const dayOfWeek = days[activeDate.getDay()];
 
-        if(appointments[staff_id].staff?.schedule && appointments[staff_id].staff?.schedule[dayOfWeek]) {
-            if(appointments[staff_id].staff?.schedule[dayOfWeek]["lunch"]) {
+        if (appointments[staff_id].staff?.schedule && appointments[staff_id].staff?.schedule[dayOfWeek]) {
+            if (appointments[staff_id].staff?.schedule[dayOfWeek]["lunch"]) {
                 const lunchSchedule = appointments[staff_id].staff?.schedule[dayOfWeek]["lunch"];
                 const timeSlotVal = parseFloat(TimeHelper.getSliderValFrom24(timeSlot));
                 return timeSlotVal >= parseFloat(lunchSchedule[0]) && timeSlotVal <= parseFloat(lunchSchedule[1]);
@@ -107,10 +122,13 @@ export default function DashboardAppointments(props) {
 
         return (
             <div {...rest} style={styles} ref={ref}>
-                <button type={"button"}>Change Date & Time</button>
-                <button type={"button"}>Modify Services</button>
+                <button type={"button"}>Check-In</button>
+                <br/>
+                <button type={"button"}>Check-Out</button>
+                <br/>
+                <button type={"button"}>Edit / Modify</button>
                 <hr/>
-                <button type={"button"}>Cancel</button>
+                <button type={"button"}>Cancel Appointment</button>
             </div>
         );
     });
@@ -133,7 +151,8 @@ export default function DashboardAppointments(props) {
                         const apps = findAppointments(staff_id, hour);
 
                         if ((!apps.length) || (apps.length && renderedApps.includes(apps[0].doc_id))) return <div
-                            className={styles.block + (isLunchBlock(staff_id, hour) ? " " + styles.lunch : "")}><h5>LUNCH</h5></div>
+                            className={styles.block + (isLunchBlock(staff_id, hour) ? " " + styles.lunch : "")}>
+                            <h5>LUNCH</h5></div>
 
                         renderedApps.push(apps[0].doc_id);
 
@@ -143,39 +162,80 @@ export default function DashboardAppointments(props) {
 
                         app.services.forEach((arg) => totalMinutes += arg.duration);
 
+                        const minuteDiff = new Date(app.start.dateTime).getMinutes() - parseInt(hour.split(":")[1]);
+                        let marginTop = (minuteDiff * (100 / 30) - 8) + "px";
+
+                        const icon = app?.check_out ? faCheckCircle : app?.check_in ? faCogs : faExclamationTriangle;
+                        const status = app?.check_out ? "Completed!" : app?.check_in ? "In progress" : "Check in"
+                        const color = app?.check_out ? styles.checked_out : app?.check_in ? styles.checked_in : styles.attention;
+
                         // if height is 100px and each 100px represent 30 minutes than 1 minute = 100 / 30 px's
                         // for the height we have to subtract 10 when padding of block is 20
                         return (
-                            <div key={staff_id + index} className={styles.block + (isLunchBlock(staff_id, hour) ? " " + styles.lunch : "")}>
+                            <div key={staff_id + index}
+                                 className={styles.block + (isLunchBlock(staff_id, hour) ? " " + styles.lunch : "")}>
                                 {apps.length && (
                                     <div className={styles.appointment} style={{
-                                        height: (totalMinutes * (100 / 30) - 10) + "px",
-                                        maxHeight: (totalMinutes * (100 / 30) - 10) + "px"
+                                        height: (totalMinutes * (100 / 30) - 7) + "px",
+                                        maxHeight: (totalMinutes * (100 / 30) - 7) + "px",
+                                        marginTop
                                     }}>
-                                        <div className={styles.leadInfo}>
-                                            <label>Client Info:</label>
-                                            <span>Name: {app.lead.name}</span>
-                                            <span>Email: {app.lead.email}</span>
-                                            <span>Phone: <button type={"button"}
-                                                                 onClick={() => toaster.push(<AuthProvider><NewTextModal
-                                                                     phone={app.lead.phone}/></AuthProvider>)}><Tag
-                                                color="blue">{app.lead.phone}</Tag></button></span>
+                                        <div className={styles.innerds}>
+
+                                            <div className={styles.infoBar}>
+                                                <div className={styles.left}>
+                                                    <span>
+                                                    {new Date(app.start.dateTime).toLocaleTimeString([], {
+                                                        hour: '2-digit',
+                                                        minute: '2-digit'
+                                                    })}
+                                                        {" - "}
+                                                        {new Date(app.end.dateTime).toLocaleTimeString([], {
+                                                            hour: '2-digit',
+                                                            minute: '2-digit'
+                                                        })}
+                                                    </span>
+
+                                                    <span className={color}>
+                                                        <FontAwesomeIcon icon={icon}/>
+                                                        {" "}
+                                                        {status}
+                                                    </span>
+                                                </div>
+                                                <Whisper
+                                                    trigger="click"
+                                                    speaker={(props, ref) => {
+                                                        const {left, top, onClose} = props;
+                                                        return <Overlay style={{left, top}} onClose={onClose}
+                                                                        className={styles.overlay + " fadeIn"}
+                                                                        ref={ref}/>;
+                                                    }}
+                                                >
+                                                    <Button className={styles.editBtn}><FontAwesomeIcon
+                                                        icon={faEllipsis}/></Button>
+                                                </Whisper>
+                                            </div>
+
+                                            <div className={styles.body}>
+                                                <div className={styles.leadInfo}>
+                                                    <label>Client Info:</label>
+                                                    <span>Name: {app.lead.name}</span>
+                                                    <span>Email: {app.lead.email}</span>
+                                                    <span>Phone: <button type={"button"}
+                                                                         onClick={() => toaster.push(
+                                                                             <AuthProvider><NewTextModal
+                                                                                 phone={app.lead.phone}/></AuthProvider>)}><Tag
+                                                        color="blue">{app.lead.phone}</Tag></button></span>
+                                                </div>
+                                                <div className={styles.services}>
+                                                    <label>Services:</label>
+                                                    {app.services.map(({name}) => <span
+                                                        key={Math.random()}>{name}</span>)}
+                                                </div>
+                                            </div>
+
+
                                         </div>
-                                        <div className={styles.services}>
-                                            <label>Services:</label>
-                                            {app.services.map(({name}) => <span key={Math.random()}>{name}</span>)}
-                                        </div>
-                                        <Whisper
-                                            trigger="click"
-                                            speaker={(props, ref) => {
-                                                const {left, top, onClose} = props;
-                                                return <Overlay style={{left, top}} onClose={onClose}
-                                                                className={styles.overlay + " fadeIn"} ref={ref}/>;
-                                            }}
-                                        >
-                                            <Button className={styles.editBtn}><FontAwesomeIcon
-                                                icon={faEllipsis}/></Button>
-                                        </Whisper>
                                     </div>
                                 )}
 
@@ -276,7 +336,7 @@ function CurrentTimeLine() {
     const getCurrentTimeOffset = () => {
         if (now.getHours() === 4) {
             return now.getMinutes() * (100 / 30);
-        } else if (now.getHours() >= 8) {
+        } else if (now.getHours() >= 20) {
             if (now.getMinutes() >= 30) {
                 return -1000;
             }
@@ -290,7 +350,8 @@ function CurrentTimeLine() {
     }
 
     return (
-        <div className={styles.currentTimeLine} style={{marginTop: (getCurrentTimeOffset()) + "px"}}>
+        <div className={styles.currentTimeLine} style={{marginTop: (getCurrentTimeOffset()) + "px"}}
+             id={"current-time-line"}>
             <span>{TimeHelper.convertTime24to12(now.getHours() + ":" + now.getMinutes())}</span>
         </div>
     )
--- pages/api/appointments.js
+++ pages/api/appointments.js
@@ -21,25 +21,21 @@ export default async function handler(req, res) {
             orderBy: 'startTime'
         });
 
-        const results = {}
+        const results = {};
 
-        if (!events.length) {
-            let allStaff = await FirebaseAdmin.getCollectionArray("staff");
-            allStaff = allStaff.filter((staff) => staff?.bookable);
-            await Promise.all(allStaff.map(async (staff) => {
-                if (!results[staff.doc_id]) {
-                    results[staff.doc_id] = {}
-                    results[staff.doc_id]["appointments"] = [];
-                    results[staff.doc_id]["staff"] = staff;
-                    const userAccount = await FirebaseAdmin.auth().getUser(staff.uid);
-                    if (userAccount?.photoURL) {
-                        results[staff.doc_id]["staff"]["photoURL"] = userAccount.photoURL;
-                    }
+        let allStaff = await FirebaseAdmin.getCollectionArray("staff");
+        allStaff = allStaff.filter((staff) => staff?.bookable);
+        await Promise.all(allStaff.map(async (staff) => {
+            if (!results[staff.doc_id]) {
+                results[staff.doc_id] = {}
+                results[staff.doc_id]["appointments"] = [];
+                results[staff.doc_id]["staff"] = staff;
+                const userAccount = await FirebaseAdmin.auth().getUser(staff.uid);
+                if (userAccount?.photoURL) {
+                    results[staff.doc_id]["staff"]["photoURL"] = userAccount.photoURL;
                 }
-            }));
-
-            return res.json(results);
-        }
+            }
+        }));
 
         await Promise.all(events.map(async (event) => {
             if (event?.extendedProperties?.private?.staff) {
@@ -60,21 +56,7 @@ export default async function handler(req, res) {
                         result[0]["services"] = await Promise.all(result[0]["services"].map(async (service_id) => (await FirebaseAdmin.firestore().collection("clover_inventory").doc(service_id).get()).data()))
                     }
 
-                    if (!results[eventStaffId]) {
-                        results[eventStaffId] = {}
-                        results[eventStaffId]["appointments"] = [result[0]];
-                        if (result[0]["staff"]) {
-                            const staffObj = await Staff.get(result[0]["staff"]);
-                            results[eventStaffId]["staff"] = staffObj;
-                            const userAccount = await FirebaseAdmin.auth().getUser(staffObj.uid);
-
-                            if (userAccount?.photoURL) {
-                                results[eventStaffId]["staff"]["photoURL"] = userAccount.photoURL;
-                            }
-                        }
-                    } else {
-                        results[eventStaffId]["appointments"].push(result[0]);
-                    }
+                    results[eventStaffId]["appointments"].push(result[0]);
                 }
             }
         }))
